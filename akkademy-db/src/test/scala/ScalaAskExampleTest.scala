926cf5a9c3fbde4f5c1599af6241df21f440e957
Pedro <plm@dazito.com>
2016-02-23 23:50:48 +0000

    Handle failure and compose futures

--- akkademy-db/src/test/scala/ScalaAskExampleTest.scala
+++ akkademy-db/src/test/scala/ScalaAskExampleTest.scala
@@ -18,7 +18,7 @@ class ScalaAskExampleTest extends FunSpecLike with Matchers {
     describe("Pong actor") {
         it("should responde with Pong") {
             val future = pongActor ? "Ping" // Uses the implicit timeout
-            // The acotr in untyped so it returns a Future[AnyRef], we use mapTo[String] to "cast" it to a Sring
+            // The actor is untyped so it returns a Future[AnyRef], we use mapTo[String] to "cast" it to a String
             val result = Await.result(future.mapTo[String], 1 second)
 
             assert(result == "Pong")
@@ -30,4 +30,45 @@ class ScalaAskExampleTest extends FunSpecLike with Matchers {
             }
         }
     }
+    
+    describe("FutureExamples") {
+        import scala.concurrent.ExecutionContext.Implicits.global
+        it("Should print to console") {
+            (pongActor ? "Ping").onSuccess({
+                case x: String => println("Replied with: " + x)
+            })
+
+            Thread.sleep(200)
+        }
+
+        it("should cause and log a failure"){
+            askPong("causeError").onFailure{
+                case e: Exception => println("Got Exception")
+            }
+        }
+
+        it("should fail and recover") {
+            val f = askPong("causeError").recover( {
+                case t: Exception => "default" // In case of error, return "default"
+            })
+        }
+
+        it("should recover from failure asynchronously") {
+            askPong("causeError").recoverWith({
+                case t: Exception => askPong("ping")
+            })
+        }
+
+        it("composing futures") {
+            askPong("ping")
+                    .flatMap(message => askPong("Ping" + message))
+                    .recover({
+                        case Exception => "There was an error"
+                    })
+        }
+    }
+
+    def askPong(message: String): Future[String] = (pongActor ? message).mapTo[String]
+
+
 }

2219b2db952102fb7a0e57433c715acf8bbb828b
Pedro <plm@dazito.com>
2016-02-25 23:01:13 +0000

    New message types: GetRequest, UnkownMessageException, KeyNotFoundException

---
+++ akkademy-db/src/main/scala/com/dazito/scala/dakkabase/messages/GetRequest.scala
@@ -0,0 +1,6 @@
+package com.dazito.scala.dakkabase.messages
+
+/**
+ * Created by daz on 25/02/2016.
+ */
+case class GetRequest (key: String) exten
---
+++ akkademy-db/src/main/scala/com/dazito/scala/dakkabase/messages/KeyNotFoundException.scala
@@ -0,0 +1,6 @@
+package com.dazito.scala.dakkabase.messages
+
+/**
+ * Created by daz on 25/02/2016.
+ */
+case class KeyNotFoundException(key: String) extends Exception

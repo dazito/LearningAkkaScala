aef8cc58205a96f3e66ecaca74e24ae1d032d212
Pedro <plm@dazito.com>
2016-02-25 23:02:16 +0000

    Implemented message handling in DakkabaseDb

--- akkademy-db/src/main/scala/com/dazito/scala/dakkabase/DakkabaseDb.scala
+++ akkademy-db/src/main/scala/com/dazito/scala/dakkabase/DakkabaseDb.scala
@@ -1,9 +1,9 @@
 package com.dazito.scala.dakkabase
 
-import akka.actor.Actor
+import akka.actor.{Status, Actor}
 import akka.actor.Actor.Receive
 import akka.event.Logging
-import com.dazito.scala.dakkabase.messages.SetRequest
+import com.dazito.scala.dakkabase.messages.{UnknownMessageException, KeyNotFoundException, GetRequest, SetRequest}
 
 import scala.collection.mutable
 
@@ -19,9 +19,21 @@ class DakkabaseDb extends Actor{
         case SetRequest(key, value) => {
             log.info("Received Set Request - key: {} | value: {}", key, value)
             map.put(key, value)
+
+            // Let the send know it succeed
+            sender() ! Status.Success
+        }
+        case GetRequest(key) => {
+            log.info("Received Get Request - key: {}", key)
+            val response: Option[Object] = map.get(key)
+            response match {
+                case Some(x) => sender() ! x
+                case None => sender() ! Status.Failure(new KeyNotFoundException(key))
+            }
         }
         case o => {
             log.info("Received unknown message: {}", o)
+            Status.Failure(new UnknownMessageException())
         }
     }
 

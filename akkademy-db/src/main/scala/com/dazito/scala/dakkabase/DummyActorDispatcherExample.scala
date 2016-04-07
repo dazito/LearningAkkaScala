bd7a7686bf0efc06afcd2df51b04da4fc9ce7ee7
Pedro <plm@dazito.com>
2016-04-08 00:05:42 +0100

    added blocking-io-dispatcher

--- akkademy-db/src/main/scala/com/dazito/scala/dakkabase/DummyActorDispatcherExample.scala
+++ akkademy-db/src/main/scala/com/dazito/scala/dakkabase/DummyActorDispatcherExample.scala
@@ -3,11 +3,24 @@ package com.dazito.scala.dakkabase
 import akka.actor.Actor
 import akka.actor.Actor.Receive
 
+import scala.concurrent.{Future, ExecutionContext}
+
 /**
  * Created by daz on 01/04/2016.
  */
 class DummyActorDispatchersExample extends Actor{
+    val blockingIoDispatcher: ExecutionContext = context.system.dispatchers.lookup("blocking-io-dispatcher")
+
     override def receive: Receive = {
-        case _ => print("Message received")
+        val s = "Hello"
+        case _ => {
+            val future: Future[String] = Future {
+                s + " future!"
+            }(blockingIoDispatcher)
+            future onSuccess {
+                case msg => println(msg)
+            }
+            print("Message received")
+        }
     }
 }
